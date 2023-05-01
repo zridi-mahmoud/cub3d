5f4f18e834604b3a3b5c26fd284b9fcb71202f52
mr rida <rida@mr-ridas-MacBook-Pro.local>
2023-05-01 17:00:37 +0100

    Add : Struct and prototype function for parcing

--- cub3d.h
+++ cub3d.h
@@ -18,6 +18,7 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <math.h>
+#include <fcntl.h>
 # define FOV 1.0472
 # define WINDOW_HEIGHT 720
 # define WINDOW_WIDTH 1280
@@ -78,6 +79,23 @@ typedef struct s_var {
 	void		*mlx_win;
 }	t_var;
 
+typedef struct s_line
+{
+    char *line;
+    struct s_line *next;
+}t_line;
+
+
+typedef struct cub3D_s
+{
+    int width;
+    int height;
+    int max_lenght;
+    char **map_arr;
+    t_line  *head;
+    int first_line;
+} t_cub3D;
+
 void	init_player(t_player *player, int i, int j, char dir);
 t_var	*init_data(char **map);
 int		check_wall(t_var *data, int i);
@@ -103,5 +121,31 @@ void	put_point(t_data *data, int x, int y, int color);
 char	get_map_value(t_var *data, t_point point);
 int		key_release(int keycode, t_var *data);
 int		mouse_move(int x, int y, t_var *data);
-
+//parce
+char	*get_next_line(int fd);
+char	*ft_strjoin(char  *s1, char  *s2);
+char	*ft_strtrim(char **s1, char  *set);
+int		ft_strlen(char *s);
+char	*get_line(int fd);
+int		ft_lstsize(t_line *lst);
+void	ft_lsttoarray(t_line *map, t_cub3D *cub);
+char	*rightpad(char *str, int n);
+void	leftpad(char *str);
+void	fill_map(t_cub3D *cub, t_line *head);
+int		ft_checkLine(char *line);
+void	fill_map_back(t_cub3D *cub, t_line *head);
+int		ft_checkForSpaces(t_cub3D *cub);
+int		ft_checkWall(t_cub3D *cub);
+int		ft_checkIfClosed(t_cub3D *cub, t_line *head);
+int		is_valid_map( char *filename);
+int		parcing(int argc, char *argv[]);
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strdup(char *src);
+// static int	ft_count(char *dest);
+char	*ft_strcat(char *dest, char *src);
+// static int	min(size_t a, size_t b);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+// static int	ft_check(char const *set, char c);
+char	*ft_strtrim(char **s1, char *set);
 #endif
